695843c49277b4cced32241eb80ac322a1bc1de8
coreylevine <coreylevine123@gmail.com>
2018-02-09 11:08:06 -0500

    Fixed comments

--- Lab Assignment 1/httpc.py
+++ Lab Assignment 1/httpc.py
@@ -4,54 +4,54 @@ from urllib.parse import urlparse
 
 
 def get(url,v):
-   # 1: is to initialize the socket
+   # 1: Initialize the socket
    listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-   # use urlparse to extract the hostname from the url
+   # Use urlparse to extract the hostname from the url
    url = urlparse(url)
    # 2: Connect to the server at port 80
    listener.connect((url.hostname, 80))
-   # 3: using GET to request from the server and \r\n\r\n to send the request
+   # 3: Use GET to request from the server and \r\n\r\n to send the request
    req = "GET " + url.path + "?" + url.query + " HTTP/1.1\r\nHost: " + url.hostname + "\r\n\r\n"
-   # 4: send out the request
+   # 4: Send out the request
    listener.sendall(req.encode("utf-8"))
+   #5: Response variable receives the response of max length 4096
    response = listener.recv(4096).decode("utf-8")
-   #5: a response variable receives the response at post 1024
 
    #If the request is a verbose type, include the 9 first lines which is the header info included in a verbose request
    if not v:
        response = response.split("\n")
        for i in range(9,len(response)):
            print(response[i])
-
    else:
        print(response)
    listener.close()
 
 def post(url, header, data, v):
-   # first step is to initialize the socket
+   # 1: Initialize the socket
    listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-   # use urlparse to extract the hostname from the url
+   # Use urlparse to extract the hostname from the url
    url = urlparse(url)
+   # 2: Connect to the server at port 80
    listener.connect((url.hostname, 80))
+   # 3: Use POST to request send information to the server and \r\n\r\n to send the request
    req = "POST " + url.path + " HTTP/1.1" + "\r\n" + "Host: " + url.hostname + "\r\n" + "Content-Length: " + str(len(data)) + "\r\n" + header + "\r\n\r\n" + data + "\r\n\r\n"
-   # 4: send out the request
+   # 4: Send out the request
    listener.sendall(req.encode("utf-8"))
+   # 5: Response variable receives the response of max length 4096
    response = listener.recv(4096).decode("utf-8")
-   #5: a response variable receives the response at post 1024
    # If the request is a verbose type, include the 9 first lines which is the header info included in a verbose request
-
    if not v:
        response = response.split("\n")
        for i in range(9, len(response)):
            print(response[i])
-
    else:
        print(response)
    listener.close()
 
 
-# argument parsing for use in the command line
+# Argument parsing for use in the command line
 parser = argparse.ArgumentParser()
+
 get_verbose_post = parser.add_mutually_exclusive_group()
 get_verbose_post.add_argument('-g', '--get', help= 'get with URL argument', type=str)
 get_verbose_post.add_argument('-p', '--post', help= 'post with URL argument', type=str)
@@ -59,8 +59,6 @@ parser.add_argument('--h', '--header', help='header for HTTP post', type=str)
 dataVsFile = parser.add_mutually_exclusive_group()
 dataVsFile.add_argument('-d', '--data', help='data string for HTTP post', type=str)
 dataVsFile.add_argument('-f', '--file', help='file for HTTP post', type=str)
-verbose = parser.add_argument('-v', '--verbose', help='show verbose output',
-       action='store_true')
 
 args = parser.parse_args()
 if args.get is not None:
